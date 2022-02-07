95af72e1ce1e45f37d2649e0d10746b2ccc08150
laubsauger <laubsauger@gmail.com>
2022-02-06 21:23:04 -0800

    try multicast membership full subnet

--- dist/config/index.js
+++ dist/config/index.js
@@ -4,11 +4,13 @@ const config = {
     // webSocketHost: 'http://localhost:8080',
     webSocketHost: 'http://192.168.1.85:8080',
     oscOverUDP: {
-        // remoteAddress: '192.168.1.85',
+        // remoteAddress: '192.168.1.255',
         // remotePort: 57121,
         localAddress: '0.0.0.0',
         localPort: 57121,
         metadata: true,
+        broadcast: true,
+        multicastMembership: '192.168.1.255:57121',
     },
 };
 exports.default = config;
