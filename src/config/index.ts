97191f04e97fbbce3aa0e0a0bcaf80345eaaa5a8
laubsauger <laubsauger@gmail.com>
2022-02-08 14:23:03 -0800

    use https

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ const config = {
   // webSocketHost: 'http://localhost:8080',
   // webSocketHost: 'http://socket.osc.link',
   // webSocketHost: 'http://147.182.251.185',
-  webSocketHost: 'http://socket.osc.link',
+  webSocketHost: 'https://socket.osc.link',
   oscOverUDP: {
     localAddress: '0.0.0.0',
     localPort: 57121,
