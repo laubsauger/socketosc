0218864238d904e086e0b6ad1514c6c775655baa
laubsauger <laubsauger@gmail.com>
2022-09-21 20:27:50 +0200

    use Hashrouter instead of BrowserRouter as electron seems to serve filebased

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { StoreProvider } from './contexts/storeContext';
 
 import "bootstrap/dist/css/bootstrap.css";
@@ -14,9 +14,9 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <StoreProvider store={new RootStore()}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </StoreProvider>
   </React.StrictMode>,
   document.getElementById('root')
